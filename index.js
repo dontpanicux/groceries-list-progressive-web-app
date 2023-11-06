a3c3f2248fdcd80c0dd53890d0d20852c24da975
Blake Arnsdorff <blake@dontpanicux.com>
2023-11-06 11:52:41 -0800

    Updated assets added initial styles
    Fixed my local assets folder issue
    Added the logo cat.png to assets folder
    Added initial layout & UI components CSS styles

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/*
+IDs I'll need
+
+*/ 
+
+// <input id="grocery-input-field" type="text" placeholder="Bread">
+/* <button id="add-to-cart-btn">Add to Cart</button> */
+
+
 console.log('js file is connected')
