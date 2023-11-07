352de94c83ffc3f4e621651f5d87f16b1cd3bbea
Blake Arnsdorff <blake@dontpanicux.com>
2023-11-07 12:35:51 -0800

    Add to & Read from Firebase functionality
    Added functions to allow for users to add items to the shopping list and maintain that list on the UI as the Firebase RTDB updates.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js'
-import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js'
+import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js'
 
 //🔥 Firebase app settings  database URL + initialize app
 
@@ -15,8 +15,9 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 
 // set the ref for our specific 'place' / location in the DB
-const shoppingListInDB = ref(database, 'shoppingList')
+const shoppingListInDB = ref(database, "shoppingList")
 // console.log(shoppingListInDB)
+
 
 // Global Vars
 
@@ -28,16 +29,41 @@ const currentShoppingList = document.getElementById('shopping-list-el')
 
 // Core functions / interactivity
 
+
 // add an event listen to our Add to Cart CTA
 addToCartBtn.addEventListener('click', function(){
+    
     //console.log('clicked the Add to Cart CTA')
     let currentInputValue = groceryInputField.value
 
-    // take currentInputValue and add it to shopplist in DB + UI
-    addToShoppingList(currentInputValue)
-
+    // Give the event listener a function that takes our input value & adds (pushes) that value to our ShoppingList DB
+    push(shoppingListInDB, currentInputValue)
+    
     // clear our input field so that a user can add more items
     clearInputField()
+
+    // take currentInputValue and add it to shopplist in DB + UI
+        // addToShoppingList(currentInputValue)
+})
+
+//onValue function -- reading data in shoppingListInDB
+onValue(shoppingListInDB, function(snapshot){
+    //clear the current page
+    clearCurrentShoppingList()
+    
+    //store the key value pairs from the DB in separate usable Vars
+    let shoppingListValues = Object.values(snapshot.val())
+    // console.log(shoppingListValues)
+    
+    let shoppingListKeys = Object.keys(snapshot.val())
+    //console.log(shoppingListKey)
+     
+    //iterate over the shoppingListValues array --> add items to UI
+    for ( let i = 0; i < shoppingListValues.length; i ++ ){
+        let currentShoppingListItem = shoppingListValues[i]
+        // currentShoppingList.innerHTML += `<li>${currentShoppingListItem}</li>`
+        addToShoppingList(currentShoppingListItem)
+    }
 })
 
 // clear input field once input value is captured
@@ -45,11 +71,13 @@ function clearInputField(){
     groceryInputField.value = ''
 }
 
+// clear currentShoppingList before updating the DB via OnValue()
+function clearCurrentShoppingList(){
+    currentShoppingList.innerHTML = ''
+}
+
 //
 function addToShoppingList(inputValue){
-    
-    // Give the event listener a function that takes our input value & adds (pushes) that value to our ShoppingList DB
-    push(shoppingListInDB, inputValue)
     
     //create a new li with currentInputValue as the content
     const newListItem = document.createElement('li')
